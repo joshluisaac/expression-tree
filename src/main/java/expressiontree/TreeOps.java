c69796834d29234d1b988c8f9ed76281bd289814
Josh Uzo <nwankwo.joshua@gmail.com>
2020-01-26 22:17:26 +1100

    Refactorings

--- src/main/java/expressiontree/TreeOps.java
+++ src/main/java/expressiontree/TreeOps.java
@@ -12,24 +12,16 @@ import expressiontree.interpreter.Interpreter;
  *     requested operations.
  */
 public class TreeOps {
-  /** Keep track of the current state of the sequence of user operations. */
+
   private State state;
-
-  /** The current @a ExpressionTree. */
   private ExpressionTree tree;
-
-  /** Returns whether or not a successful format call has been called. */
   private boolean formatted;
-
-  /** The interpreter used to parse and process user expression input. */
   private Interpreter interpreter;
 
-  /** Accessor for the interpreter. */
   public Interpreter interpreter() {
     return interpreter;
   }
 
-  /** Ctor */
   public TreeOps() {
     state = new UninitializedState();
     formatted = false;
@@ -37,48 +29,27 @@ public class TreeOps {
     tree = new ExpressionTree(null);
   }
 
-  /** Set the desired format to the designated @a newformat. */
-  public void format(String newformat) {
-    state.format(this, newformat);
+  public void format(String newFormat) {
+    state.format(this, newFormat);
     this.formatted = true;
   }
 
-  /**
-   * Make an expression tree based on the designated @a expression using the previously designated
-   * format.
-   */
   public void makeTree(String expression) {
     state.makeTree(this, expression);
   }
 
-  /** Print the most recently created expression tree using the designated @a format. */
   public void print(String format) {
     state.print(this, format);
   }
 
-  /**
-   * Evaluate the "yield" of the most recently created expression tree using the designated @a
-   * format.
-   */
   public void evaluate(String format) {
     state.evaluate(this, format);
   }
 
-  /** Sets the variable to its corresponding value. */
   public void set(String keyValuePair) throws Exception {
-    /** Get rid of all spaces. */
     String inputString = keyValuePair.replaceAll(" ", "");
-
-    /** Keep track of found characters. */
     int pos;
-
-    /** Inputstring should be key=value. */
     if ((pos = inputString.indexOf('=')) != -1) {
-      /**
-       * If the position is not the first char (e.g., '=value') and position is not the last char
-       * (e.g., 'key=') then split the string and set the symbol table in the interpreter
-       * accordingly.
-       */
       if (pos != 0 && pos < inputString.length() - 1) {
         String key = inputString.substring(0, pos);
         String value = inputString.substring(pos + 1);
@@ -88,27 +59,22 @@ public class TreeOps {
     } else throw new Exception("Must have = sign present");
   }
 
-  /** Return a pointer to the current @a State. */
   State state() {
     return state;
   }
 
-  /** Set the current @a State to the designated @a newstate pointer. */
   void state(State newState) {
     this.state = newState;
   }
 
-  /** Return the current @a ExpressionTree that's owned by this object. */
   ExpressionTree tree() {
     return tree;
   }
 
-  /** Set the current @a ExpressionTree to @a newtree. */
   void tree(ExpressionTree newTree) {
     tree = newTree;
   }
 
-  /** Returns whether or not a successful format call has been called. */
   public final boolean formatted() {
     return formatted;
   }
